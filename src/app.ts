173cc9b9e2486acb4d0b1ef16dbce151544e6886
Omid Golshan <omid.gt21@gmail.com>
2023-10-28 21:22:57 +0200

    Implement a `TaskExecution` context

---
+++ src/app.ts
@@ -0,0 +1,60 @@
+import { CliApplication, Task, Command, Arg } from './core';
+
+const delay = (ms: number) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      try {
+        resolve(null);
+      } catch (e) {
+        reject(e);
+      }
+    }, ms);
+  });
+};
+
+class MyTask extends Task<void, string> {
+  async execute(): Promise<string> {
+    await delay(1000);
+    return 'Hello from MyTask!';
+  }
+}
+
+class AnotherTask extends Task<string, void> {
+  async execute(input: string): Promise<void> {
+    await delay(3000);
+    throw new Error('bo bo bo');
+    console.log(input);
+  }
+}
+
+class MyCliApp extends CliApplication {
+  constructor() {
+    super({ version: '1.0.0' });
+  }
+
+  @Command({ description: 'Runs MyTask and then AnotherTask' })
+  async runTasks(): Promise<void> {
+    const myTask = new MyTask(this.spinner);
+    const anotherTask = new AnotherTask(this.spinner);
+    const result = await myTask.run();
+    await anotherTask.run(result);
+  }
+
+  @Command({ description: 'Just says hello' })
+  sayHello(
+    @Arg('name', 'the name of person') name: string,
+    @Arg('tag', 'whether it has a tag or not') hasTag: boolean,
+  ): void {
+    console.log(
+      `Hello from ${name ?? 'App'} and it ${hasTag ? 'has' : 'has not'} tag!`,
+    );
+  }
+
+  @Command({ description: 'Will call work' })
+  nicklas(): void {
+    console.log('Hello from Nicklas!');
+  }
+}
+
+const app = new MyCliApp();
+app.run();
